2300af611e8798bfbfb777e48bca3a6659d5c29b
OPC-16 <chavan.p.omkar@gmail.com>
2024-06-16 12:52:56 +0530

    Create Hello world

---
+++ flask-orders-api/__init__.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+            SECRET_KEY='dev',
+            DATABASE=os.path.join(app.instance_path, 'flask-orders-api.sqlite'),
+            )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    # ensure the instance directory exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page to display hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
